167ddc7279c80cc255a78ee085a1923c0e33e3ee
Kostya Deev <kostya.deev@bluware.com>
2020-10-25 13:52:17 -0500

    resources

---
+++ tiles_sheet.tsx
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tiles_sheet" tilewidth="64" tileheight="64" tilecount="96" columns="16" backgroundcolor="#000000">
+ <image source="pirate-pack/Tilesheet/tiles_sheet.png" trans="000000" width="1024" height="384"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="17"/>
+  <terrain name="New Terrain" tile="22"/>
+  <terrain name="New Terrain" tile="26"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="0,0,0,"/>
+ <tile id="4" terrain="0,0,,0"/>
+ <tile id="5" terrain=",,,1"/>
+ <tile id="6" terrain=",,1,1"/>
+ <tile id="7" terrain=",,1,1"/>
+ <tile id="8" terrain=",,1,"/>
+ <tile id="9" terrain=",,,2"/>
+ <tile id="10" terrain=",,2,2"/>
+ <tile id="11" terrain=",,2,"/>
+ <tile id="16" terrain=",0,,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,,0,"/>
+ <tile id="19" terrain="0,,0,0"/>
+ <tile id="20" terrain=",0,0,0"/>
+ <tile id="21" terrain=",1,,1"/>
+ <tile id="22" terrain="1,1,1,1"/>
+ <tile id="23" terrain="1,1,1,1"/>
+ <tile id="24" terrain="1,,1,"/>
+ <tile id="25" terrain=",2,,2"/>
+ <tile id="26" terrain="2,2,2,2"/>
+ <tile id="27" terrain="2,,2,"/>
+ <tile id="32" terrain=",0,,"/>
+ <tile id="33" terrain="0,0,,"/>
+ <tile id="34" terrain="0,,,"/>
+ <tile id="35" terrain="1,1,1,"/>
+ <tile id="36" terrain="1,1,,1"/>
+ <tile id="37" terrain=",1,,1"/>
+ <tile id="38" terrain="1,1,1,1"/>
+ <tile id="39" terrain="1,1,1,1"/>
+ <tile id="40" terrain="1,,1,"/>
+ <tile id="41" terrain=",2,,"/>
+ <tile id="42" terrain="2,2,,"/>
+ <tile id="43" terrain="2,,,"/>
+ <tile id="51" terrain="1,,1,1"/>
+ <tile id="52" terrain=",1,1,1"/>
+ <tile id="53" terrain=",1,,"/>
+ <tile id="54" terrain="1,1,,"/>
+ <tile id="55" terrain="1,1,,"/>
+ <tile id="56" terrain="1,,,"/>
+ <tile id="57" terrain="2,2,2,"/>
+ <tile id="58" terrain="2,2,,2"/>
+ <tile id="73" terrain="2,,2,2"/>
+ <tile id="74" terrain=",2,2,2"/>
+ <wangsets>
+  <wangset name="New Wang Set" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="12" wangid="0x1010101"/>
+   <wangtile tileid="13" wangid="0x1010101"/>
+   <wangtile tileid="14" wangid="0x1020102"/>
+   <wangtile tileid="15" wangid="0x2010201"/>
+   <wangtile tileid="28" wangid="0x1020102"/>
+   <wangtile tileid="29" wangid="0x2010201"/>
+   <wangtile tileid="44" wangid="0x1020101"/>
+   <wangtile tileid="45" wangid="0x1010201"/>
+   <wangtile tileid="60" wangid="0x1010102"/>
+   <wangtile tileid="61" wangid="0x2010101"/>
+   <wangtile tileid="76" wangid="0x1020201"/>
+   <wangtile tileid="77" wangid="0x2020101"/>
+   <wangtile tileid="92" wangid="0x1010202"/>
+   <wangtile tileid="93" wangid="0x2010102"/>
+  </wangset>
+ </wangsets>
+</tileset>
