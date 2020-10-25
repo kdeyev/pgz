8a086a14dd8ad7c5befe57aa05564640cbd1fa1f
Kostya Deev <kostya.deev@bluware.com>
2020-10-25 14:34:39 -0500

    collisions

--- tiles_sheet.tsx
+++ tiles_sheet.tsx
@@ -6,23 +6,100 @@
   <terrain name="New Terrain" tile="22"/>
   <terrain name="New Terrain" tile="26"/>
  </terraintypes>
- <tile id="0" terrain=",,,0"/>
- <tile id="1" terrain=",,0,0"/>
- <tile id="2" terrain=",,0,"/>
- <tile id="3" terrain="0,0,0,"/>
- <tile id="4" terrain="0,0,,0"/>
- <tile id="5" terrain=",,,1"/>
+ <tile id="0" terrain=",,,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.09091" y="63.8182">
+    <polygon points="0,0 1.81818,-27.2727 13.0909,-44.9091 39.8182,-55.6364 62.7273,-61.2727 62.7273,-0.727273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1" terrain=",,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="64.1818" y="2.36364">
+    <polygon points="0,0 -18.5455,2 -37.8182,-0.727273 -57.4545,-0.727273 -64.5455,-0.727273 -64,61.2727 -0.545455,61.4545"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2" terrain=",,0,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.545455" y="1.81818">
+    <polygon points="0,0 22.5455,0.363636 37.8182,7.45455 48,20.5455 53.0909,33.6364 61.2727,61.0909 62,62.3636 0.181818,62"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain="0,0,0,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.545455" y="0.181818">
+    <polygon points="0,0 62.9091,0 63.4545,63.2727 -0.545455,63.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain="0,0,,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 63.6364,-0.545455 64,63.4545 0.363636,63.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5" terrain=",,,1">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="1.63636" y="63.8182">
+    <polygon points="0,0 -0.181818,-24.7273 10.5455,-42 25.4545,-50.5455 43.4545,-56 64.0303,-62.1515 63.697,0.181818"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="6" terrain=",,1,1"/>
- <tile id="7" terrain=",,1,1"/>
- <tile id="8" terrain=",,1,"/>
+ <tile id="7" terrain=",,1,1">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="-0.545455" y="0.363636">
+    <polygon points="0,0 31.0909,2.18182 54.7273,4 64.7273,1.81818 64.5455,63.2727 0.727273,63.4545"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8" terrain=",,1,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.909091" y="2.36364">
+    <polygon points="0,0 21.4545,1.09091 37.0909,4.90909 49.8182,17.8182 58,41.4545 63.8182,62 0.181818,61.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="9" terrain=",,,2"/>
  <tile id="10" terrain=",,2,2"/>
  <tile id="11" terrain=",,2,"/>
- <tile id="16" terrain=",0,,0"/>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32.1818" y="32.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="16" terrain=",0,,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="1.45455">
+    <polygon points="0,0 2.90909,14.9091 -0.545455,34.5455 -0.363636,61.4545 61.6364,62.1818 61.8182,-0.727273 61.8182,-1.63636"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="17" terrain="0,0,0,0"/>
- <tile id="18" terrain="0,,0,"/>
- <tile id="19" terrain="0,,0,0"/>
- <tile id="20" terrain=",0,0,0"/>
+ <tile id="18" terrain="0,,0,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="62.7273" y="0.909091">
+    <polygon points="0,0 0.181818,18 -2,35.0909 -3.27273,47.6364 -1.09091,62.9091 -62.5455,63.0909 -62.7273,-0.545455"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19" terrain="0,,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.181818" y="0.181818">
+    <polygon points="0,0 63.0909,-0.181818 63.4545,63.2727 0.181818,63.2727"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20" terrain=",0,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.727273" y="0.363636">
+    <polygon points="0,0 62.7273,-0.545455 63.4545,63.0909 -0.545455,63.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="21" terrain=",1,,1"/>
  <tile id="22" terrain="1,1,1,1"/>
  <tile id="23" terrain="1,1,1,1"/>
@@ -30,9 +107,37 @@
  <tile id="25" terrain=",2,,2"/>
  <tile id="26" terrain="2,2,2,2"/>
  <tile id="27" terrain="2,,2,"/>
- <tile id="32" terrain=",0,,"/>
- <tile id="33" terrain="0,0,,"/>
- <tile id="34" terrain="0,,,"/>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32" y="32.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32.3636" y="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="32" terrain=",0,,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.909091" y="0.363636">
+    <polygon points="0,0 8.90909,30 18.5455,48.9091 29.4545,57.0909 35.6364,59.6364 62.3636,62.1818 62.5455,-0.181818"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33" terrain="0,0,,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.545455" y="61.0909">
+    <polygon points="0,0 18.3636,-1.81818 42.9091,1.63636 62.7273,0.909091 63.4545,-60.9091 -0.727273,-60.5455"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34" terrain="0,,,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="62.9091" y="1.27273">
+    <polygon points="0,0 -1.81818,24.3636 -9.63636,41.4545 -40.5455,53.8182 -61.8182,60.9091 -62,-0.727273"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="35" terrain="1,1,1,"/>
  <tile id="36" terrain="1,1,,1"/>
  <tile id="37" terrain=",1,,1"/>
@@ -42,6 +147,16 @@
  <tile id="41" terrain=",2,,"/>
  <tile id="42" terrain="2,2,,"/>
  <tile id="43" terrain="2,,,"/>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32.1818" y="31.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32.7273" y="32"/>
+  </objectgroup>
+ </tile>
  <tile id="51" terrain="1,,1,1"/>
  <tile id="52" terrain=",1,1,1"/>
  <tile id="53" terrain=",1,,"/>
@@ -50,8 +165,38 @@
  <tile id="56" terrain="1,,,"/>
  <tile id="57" terrain="2,2,2,"/>
  <tile id="58" terrain="2,2,,2"/>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32.1818" y="32.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32.1818" y="32.5455"/>
+  </objectgroup>
+ </tile>
  <tile id="73" terrain="2,,2,2"/>
  <tile id="74" terrain=",2,2,2"/>
+ <tile id="76">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32.1818" y="32.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="31.8182" y="33.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="92">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="cannon" x="32" y="32.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup draworder="index" id="3">
+   <object id="3" type="cannon" x="32.3636" y="33.0909"/>
+  </objectgroup>
+ </tile>
  <wangsets>
   <wangset name="New Wang Set" tile="-1">
    <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
