15bf32f4f60692420b39aaf88ff531a1133de838
Kostya Deev <kostya.deev@bluware.com>
2020-10-30 09:49:58 -0500

    building handshake

--- pgz/multiplayer_scene.py
+++ pgz/multiplayer_scene.py
@@ -28,13 +28,25 @@ class MultiplayerActor(Actor):
 
         super().__setattr__(attr, value)
 
+    def get_state_message(self):
+        state = {}
+        for attr in self.__class__.DELEGATED_ATTRIBUTES:
+            try:
+                value = getattr(self, attr)
+            except Exception as e:
+                print(f"MultiplayerActor.get_state_message: {e}")
+            state[attr] = value
+        return state
+
 
 class MultiplayerClientHeadlessScene(EventDispatcher):
     def __init__(self):
         super().__init__()
+        self.uuid = None
         self.map = None
         self.broadcast = None
         self.actor_factory = {}
+        self.actors = {}
         self.rpc = Registrator()
         self.keyboard = Keyboard()
 
@@ -48,14 +60,25 @@ class MultiplayerClientHeadlessScene(EventDispatcher):
             elif event.type == pygame.KEYUP:
                 self.keyboard._release(event.key)
 
-            self.dispatch_event(event)
+            try:
+                self.dispatch_event(event)
+            except Exception as e:
+                print(f"handle_client_event: {e}")
 
     def handle_message(self, message):
-        self.rpc.dispatch(message)
+        try:
+            self.rpc.dispatch(message)
+        except Exception as e:
+            print(f"handle_message: {e}")
+
+    def on_enter(self, previous_scene=None):
+        pass
+
+    def on_exit(self, next_scene=None):
+        pass
 
     def handle_event(self, event):
         pass
-        # super().handle_event(event)
 
     def update(self, dt):
         self.map.update(dt)
@@ -65,10 +88,11 @@ class MultiplayerClientHeadlessScene(EventDispatcher):
         self.broadcast(message)
 
     def create_actor(self, cls_name, *args, **kwargs):
-        uuid = str(uuid4())
+        uuid = f"{cls_name}-{str(uuid4())}"
         actor = self.actor_factory[cls_name](uuid, self.remove_actor, *args, **kwargs)
         actor.keyboard = self.keyboard
         actor._on_prop_change = self.broadcast_property_change
+        self.actors[uuid] = actor
         self.map.add_sprite(actor)
 
         message = serialize_json_rpc("create_actor_on_client", (uuid, actor.image_name))
@@ -77,9 +101,16 @@ class MultiplayerClientHeadlessScene(EventDispatcher):
         return actor
 
     def remove_actor(self, actor):
-        uuid = str(uuid4())
+        self.map.remove_sprite(actor)
+
+        uuid = actor.uuid
+        del self.actors[uuid]
         message = serialize_json_rpc("remove_actor_on_client", (uuid))
         self.broadcast(message)
+
+    def remove_all_actors(self):
+        for actor in self.actors.values():
+            self.remove_actor(actor)
 
 
 class MultiplayerSceneServer:
@@ -98,35 +129,58 @@ class MultiplayerSceneServer:
         if self.clients:  # asyncio.wait doesn't accept an empty list
             await asyncio.wait([client.send(message) for client in self.clients])
 
-    def remove_actor(self, actor):
-        self.map.remove_actor(actor)
+    # def remove_actor(self, actor):
+    #     self.map.remove_actor(actor)
+
+    async def _send_handshake(self, websocket, uuid):
+        actors = []
+        for scene in self.clients.values():
+            actors += scene.actors.values()
+
+        actors_states = {actor.uuid: actor.get_state_message() for actor in actors}
+        massage = {"uuid": uuid, "actors_states": actors_states}
+        massage = json.dumps(massage)
+        await websocket.send(massage)
 
     async def _register_client(self, websocket):
+        uuid = f"{self.HeadlessSceneClass.__name__}-{str(uuid4())}"
+
         scene = self.HeadlessSceneClass()
         scene.map = self.map
         scene.broadcast = self.broadcast
+        scene.uuid = uuid
+
+        await self._send_handshake(websocket, uuid)
 
         self.clients[websocket] = scene
-
         scene.on_enter(None)
-        # await notify_users()
 
     async def _unregister_client(self, websocket):
         scene = self.clients[websocket]
+
+        # First of all remove dead client
+        del self.clients[websocket]
+
         scene.on_exit(None)
-        self.clients.remove(websocket)
+        scene.remove_all_actors()
         # await notify_users()
 
     def _handle_client_message(self, websocket, message):
-        self.clients[websocket].handle_message(message)
+        try:
+            self.clients[websocket].handle_message(message)
+        except Exception as e:
+            print(f"_handle_client_message: {e}")
 
     async def _serve_client(self, websocket, path):
         # register(websocket) sends user_event() to websocket
         await self._register_client(websocket)
         try:
             async for message in websocket:
-                message = json.loads(message)
-                self._handle_client_message(websocket, message)
+                try:
+                    message = json.loads(message)
+                    self._handle_client_message(websocket, message)
+                except Exception as e:
+                    print(f"_serve_client: {e}")
         finally:
             await self._unregister_client(websocket)
 
@@ -143,7 +197,7 @@ class MultiplayerClient(Scene):
         super().__init__()
         self.map = map
         self.actors = {}
-        # self.client_uuid = str(uuid4())
+        self.uuid = None
         self._websocket = None
         self.rpc = Registrator()
 
@@ -170,6 +224,7 @@ class MultiplayerClient(Scene):
 
         @self.rpc.register
         def on_actor_prop_change(uuid, prop, value):
+            print(f"MultiplayerClient.on_actor_prop_change {self.uuid}: {uuid} {prop}: {value}")
             actor = self.actors[uuid]
             setattr(actor, prop, value)
 
@@ -177,16 +232,41 @@ class MultiplayerClient(Scene):
         await self._websocket.send(message)
 
     def handle_event(self, event):
+        if not self._websocket:
+            # not connected yet
+            return
+
         if event.type in [pygame.MOUSEMOTION, pygame.KEYDOWN, pygame.KEYUP]:
+            print(f"MultiplayerClient.handle_event {self.uuid}: {event.__class__.__name__} {event.type} {event.__dict__}")
             message = serialize_json_rpc("handle_client_event", (event.type, event.__dict__))
             asyncio.ensure_future(self._send_message(message))
             # self._websocket.send(message)
 
     async def connect_to_server(self, host: str = "localhost", port: int = 8765):
-        self._websocket = await websockets.connect(f"ws://{host}:{port}")
-        asyncio.ensure_future(self._handle_messages())
+        websocket = await websockets.connect(f"ws://{host}:{port}")
+        await self._receive_handshake(websocket)
+        asyncio.ensure_future(self._handle_messages(websocket))
 
-    async def _handle_messages(self):
+    async def _receive_handshake(self, websocket):
+        massage = await websocket.recv()
+        massage = json.loads(massage)
+
+        self.uuid = massage["uuid"]
+        actors_states = massage["actors_states"]
+        for uuid, state in actors_states.items():
+            image_name = state["image_name"]
+            actor = Actor(image_name)
+            for attr, value in state.items():
+                setattr(actor, attr, value)
+
+            self.actors[uuid] = actor
+            self.map.add_sprite(actor)
+
+    async def _handle_messages(self, websocket):
+        self._websocket = websocket
         async for message in self._websocket:
-            message = json.loads(message)
-            self.rpc.dispatch(message)
+            try:
+                message = json.loads(message)
+                self.rpc.dispatch(message)
+            except Exception as e:
+                print(f"_handle_messages: {e}")
